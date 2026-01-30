70cbdf30dcee71bd28a9725eb1aaf046b5951a5f
NorSomething <mcnirmalkumar00@gmail.com>
2026-01-30 20:14:23 +0530

    added basic saving and backspace

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <unistd.h>
 #include <stdio.h>
 
-// ctrl + o to save
+// ctrl + o to save = 15
+// backsapce =  127 or 8 (terminal dependent?)
 
 
 struct termios orig;
@@ -27,27 +28,44 @@ int main() {
 
     FILE *fptr;
 
-    fptr = fopen("test.txt", "a");
+    fptr = fopen("pp.txt", "w");
 
     if (fptr == NULL) {
         printf("File Opening Error.");
     }
 
-
+    char line_buffer[10000];
+    int l = 0;
     char c;
+
     while (read(STDIN_FILENO, &c, 1) == 1) { // reads one byte
         //printf("Key: %d\n", c); 
 
         write(STDOUT_FILENO, &c, 1);
         
+        if (l < sizeof(line_buffer)) {
+            line_buffer[l++] = c;
+        }
+        
         if (c == 27) {
 
             fclose(fptr);
-
             break;   // ESC quits
         }
-        //  printf("%c", c);
-        fputc(c, fptr);
+
+        if (c == 127 || c == 8) { // backspace
+
+            if (l > 0) l--;
+            write(STDOUT_FILENO, "\b \b", 3); // ascii backspace
+ 
+        }
+
+        if (c == 15) {
+            fwrite(line_buffer, 1, l, fptr); // fputs wants null ternimated string but we need raw bytes?
+            fflush(fptr);
+        }
+
+        
 
         
 
